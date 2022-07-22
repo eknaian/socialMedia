40638e038e66a3082c070ee85b251905b2568595
eknaian <eknaian@gmail.com>
2022-07-22 08:33:06 -0400

    init commit, working on server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extender: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extender: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://ChrisEknaian:ChrisEknaian2358@cluster0.yfnjv.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5001;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
